3c6d6fb0b0bdd3df2e9d74b5dbf564f75fdc2c44
Poncho <ameneses67@gmail.com>
2023-04-08 17:29:40 -0600

    migración a async/await

--- src/ts/consultarCotizacion.ts
+++ src/ts/consultarCotizacion.ts
@@ -19,16 +19,25 @@ export interface Moneda {
   LASTUPDATE: string;
 }
 
-export function consultarCotizacion() {
+export async function consultarCotizacion() {
   const { moneda, criptomoneda } = objBusqueda;
 
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
   mostrarSpinner();
 
-  fetch(url)
-    .then((respuesta): Promise<Display> => respuesta.json())
-    .then((cotizacion) => {
-      mostrarCotizacion(cotizacion.DISPLAY[criptomoneda][moneda]);
-    });
+  // fetch(url)
+  //   .then((respuesta): Promise<Display> => respuesta.json())
+  //   .then((cotizacion) => {
+  //     mostrarCotizacion(cotizacion.DISPLAY[criptomoneda][moneda]);
+  //   });
+
+  // async/await
+  try {
+    const respuesta = await fetch(url);
+    const cotizacion = await respuesta.json();
+    mostrarCotizacion(cotizacion.DISPLAY[criptomoneda][moneda]);
+  } catch (error) {
+    console.log(error);
+  }
 }
